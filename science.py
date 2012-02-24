c9a6dd5b8d8a9eee563973e0db686e49b04ae7a7
mrdmnd <mrdmnd@mit.edu>
2012-02-24 13:45:05 -0500

    Removed some cruft.

--- science.py
+++ science.py
@@ -21,9 +21,8 @@ fpsClock = pygame.time.Clock()
 resolution = (640,480)
 
 windowSurfaceObj = pygame.display.set_mode(resolution)
-pygame.display.set_caption('Out Dump Science Test')
+pygame.display.set_caption('Science!')
 
-drawingSurfaceObj = pygame.image.load('media/images/drawing.png')
 characterObj = pygame.image.load('media/images/etymology_man.png')
 redColor = pygame.Color(255,0,0)
 greenColor = pygame.Color(0,255,0)
@@ -31,26 +30,14 @@ blueColor = pygame.Color(0,0,255)
 whiteColor = pygame.Color(255,255,255)
 mousex, mousey = 0, 0
 
-fontObj = pygame.font.Font('freesansbold.ttf', 32)
-msg = 'Hello World! This is out Science! game'
 
 map = maps.Map(filename=mapname)
 
 while True:
     windowSurfaceObj.fill(whiteColor)
-
-    pygame.draw.rect(windowSurfaceObj, redColor, (10,10,50,100))
-    windowSurfaceObj.blit(drawingSurfaceObj, (0, 0))
-    
     map.render(windowSurfaceObj)
 
     windowSurfaceObj.blit(characterObj, (mousex, mousey))
-
-    msgSurfaceObj = fontObj.render(msg, False, blueColor)
-    msgRectangleObj = msgSurfaceObj.get_rect()
-    msgRectangleObj.topleft = (10,20)
-    windowSurfaceObj.blit(msgSurfaceObj, msgRectangleObj)
-
 
     for event in pygame.event.get():
         if event.type == QUIT:
